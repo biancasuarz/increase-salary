039f28fabfb551338f73afff14da3d15b26b4109
biancasuarz <blancasuarz@gmail.com>
2024-09-25 13:14:23 -0300

    final project

---
+++ src/Application/Program.java
@@ -0,0 +1,67 @@
+package Application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("How many employees will be registered?");
+        int N = sc.nextInt();
+
+        for (int i=0; i<N; i++){
+
+            System.out.println();
+            System.out.println("Employee #" + (i + 1) + ":" );
+            System.out.print("Id: ");
+            Integer id = sc.nextInt();
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            Double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+
+            list.add(emp);
+        }
+
+        System.out.println();
+        System.out.print("Enter the employee id that have salary increase: ");
+        int idsalary = sc.nextInt();
+        Integer pos = positionId(list, idsalary);
+        if (pos == null) {
+            System.out.println("This id does not exist!");
+        }
+        else{
+            System.out.print("Enter the percentage: ");
+            Double percent = sc.nextDouble();
+            list.get(pos).increaseSalary(percent);
+        }
+        System.out.println();
+        System.out.println("List of employees: ");
+        for (Employee emp : list){
+            System.out.println(emp);
+        }
+
+        sc.close();
+    }
+
+    public static Integer positionId(List<Employee> list, int id){
+        for (int i=0; i < list.size();i++){
+            if (list.get(i).getId() == id){
+                return i;
+            }
+        }
+        return null;
+    }
+
+}
